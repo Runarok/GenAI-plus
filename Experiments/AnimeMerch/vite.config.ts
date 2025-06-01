ec1c1bdf9074737942b36a0f97455e2bd41f77cb
TempestAethel <shreeneethreddy@gmail.com>
2025-06-01 18:19:35 +0530

    Error Debug ?

--- Experiments/AnimeMerch/vite.config.ts
+++ Experiments/AnimeMerch/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/GenAI-plus/Experiments/AnimeMerch/',  // IMPORTANT: set your repo and folder path here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
