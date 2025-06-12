38b97ab6155e7c44abe84d38c523c44656bb4fe9
Runarok <RunarokHrafn@gmail.com>
2025-06-12 06:39:30 +0530

    Update quotes-data.js

--- Experiments/Quotes/js/quotes-data.js
+++ Experiments/Quotes/js/quotes-data.js
@@ -7,27 +7,45 @@ const quotes = [
         url: "https://www.youtube.com/shorts/I3ZKegVC9R4"
     },
     {
-        quote: "Innovation distinguishes between a leader and a follower.",
-        author: "Steve Jobs",
-        source: "Apple Inc.",
-        url: "https://www.apple.com/steve-jobs-quotes/"
+        quote: "Indifference and neglect often do much more damage than outright dislike.",
+        author: "J.K. Rowling",
+        source: "goodreads",
+        url: "https://www.goodreads.com/author/show/1077326.J_K_Rowling"
     },
     {
-        quote: "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.",
-        author: "Steve Jobs",
-        source: "Apple Inc.",
-        url: "https://www.apple.com/steve-jobs-quotes/"
+        quote: "Peace is not the absence of chaos — it’s learning what deserves your reaction.",
+        author: "Runarok Hrafn",
+        source: "Own",
+        url: "https://github.com/Runarok"
     },
     {
-        quote: "Always remember that you are absolutely unique. Just like everyone else.",
-        author: "Margaret Mead",
-        source: "My Own Quote",
-        url: "https://en.wikipedia.org/wiki/Margaret_Mead"
+        quote: "Be the change that you wish to see in the world.",
+        author: "Mahatma Gandhi",
+        source: "goodreads",
+        url: "https://www.goodreads.com/author/show/5810891.Mahatma_Gandhi"
     },
     {
-        quote: "The best way to predict the future is to invent it.",
-        author: "Alan Kay",
-        source: "My Own Quote",
-        url: "https://en.wikipedia.org/wiki/Alan_Kay"
+        quote: "Growth is when you stop needing to win every argument and start choosing peace over ego.",
+        author: "Runarok Hrafn",
+        source: "Own",
+        url: "https://github.com/Runarok"
+    },
+    {
+        quote: "Effort speaks louder than words — especially when no one asked for it.",
+        author: "Runarok Hrafn",
+        source: "Own",
+        url: "https://github.com/Runarok"
+    },
+    {
+        quote: "If you tell the truth, you don't have to remember anything.",
+        author: "Mark Twain",
+        source: "goodreads",
+        url: "https://www.goodreads.com/author/show/1244.Mark_Twain"
+    },
+    {
+        quote: "Even if the pace is slow, win in a way that reflects growth — not reaction. Because true evolution isn't loud, it's mindful.",
+        author: "Runarok Hrafn",
+        source: "Own",
+        url: "https://github.com/Runarok"
     }
 ];
