a6dd23480f2db95bedc96302d63476d169ac5639
TempestAethel <shreeneethreddy@gmail.com>
2025-12-30 06:45:11 +0530

    LocalStorage Viewer

--- assets/Data.js
+++ assets/Data.js
@@ -48,6 +48,7 @@ const menuData = [
       { label: "Markdown Editor", url: "Tools/MarkDown/index.html", desc: "Live Github markdown." },
       { label: "SVG Editor", url: "Tools/Svg Edit/index.html", desc: "Live SVG Editor." },
       { label: "Morse Code", url: "Tools/MorseCode/index.html",  desc: "Live MorseCode." },
+      { label: "LocalStorage  Viewer", url: "Tools/LocalStorage/index.html",  desc: "View LocalStorage." },
       { label: "Matrix Generator", url: "Tools/Matrix_Gen/index.html",  desc: "Random Matrix Generator." },
       { label: "Brightness Gradient", url: "Tools/Brightness Gradient/index.html",  desc: "Brightness Gradient Image Editor." },
       { label: "Themes", url: "Tools/Theme/index.html", desc: "Few Themes." }
